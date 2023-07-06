34dab7d6057bb1769c0754cfd0fa2624e6ac658f
tngtied <tngtied@gmail.com>
2023-07-07 01:44:20 +0900

    Initial commit

---
+++ triplaner/src/main/java/com/tngtied/triplaner/TriplanerApplication.java
@@ -0,0 +1,13 @@
+package com.tngtied.triplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriplanerApplication.class, args);
+	}
+
+}
---
+++ triplaner/src/test/java/com/tngtied/triplaner/TriplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tngtied.triplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
