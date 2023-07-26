2058a63c7f6d3b44278f35d878729951d439644e
조유성 <yuseong.cho@toss.im>
2023-07-27 01:16:09 +0900

    불필요한 diff 제거

--- triplaner/src/main/java/com/tngtied/triplaner/dayplan_repository.java
+++ triplaner/src/main/java/com/tngtied/triplaner/dayplan_repository.java
@@ -1,12 +1,8 @@
 package com.tngtied.triplaner;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-
 @Repository
-public interface dayplan_repository extends JpaRepository<DayPlan, Integer> {
+public interface dayplan_repository extends CrudRepository<DayPlan, Integer> {
 }
--- triplaner/src/main/java/com/tngtied/triplaner/plan_repository.java
+++ triplaner/src/main/java/com/tngtied/triplaner/plan_repository.java
@@ -1,15 +1,13 @@
 package com.tngtied.triplaner;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public interface plan_repository extends JpaRepository<Plan, Integer> {
+public interface plan_repository extends CrudRepository<Plan, Integer> {
 
 
     public static final String FIND_THUMBNAILS = "SELECT new com.tngtied.triplaner.TripThumbnailDTO(p.TITLE, p.START_DATE, p.END_DATE) FROM Plan p";
