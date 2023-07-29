892f58e92cd0c5fd89395d9c5e9d28c15e77f44d
조유성 <yuseong.cho@toss.im>
2023-07-30 04:57:13 +0900

    @NonNull annotation은 오히려 헷갈리게 만드는 것 같다

--- triplaner/src/main/java/com/tngtied/triplaner/InitiateTripRequestDTO.java
+++ triplaner/src/main/java/com/tngtied/triplaner/InitiateTripRequestDTO.java
@@ -7,21 +7,17 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class InitiateTripRequestDTO {
 
-    @NonNull
     public String title;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @NonNull
     public Date startDate;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @NonNull
     public Date endDate;
 }
--- triplaner/src/main/java/com/tngtied/triplaner/TripThumbnailDTO.java
+++ triplaner/src/main/java/com/tngtied/triplaner/TripThumbnailDTO.java
@@ -4,20 +4,15 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import lombok.NonNull;
-
 public interface TripThumbnailDTO {
 
     long getPlanId();
 
-    @NonNull
     String getTitle();
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @NonNull
     Date getStartDate();
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @NonNull
     Date getEndDate();
 }
