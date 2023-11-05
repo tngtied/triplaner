4ecce885fa2440f2277fb42cb7c1cec83df320b5
tngtied <tngtied@gmail.com>
2023-11-05 15:37:48 +0900

    duplicated implementations of ```userDetailsService``` removed

--- triplaner/src/main/java/com/tngtied/triplaner/service/UserService.java
+++ triplaner/src/main/java/com/tngtied/triplaner/service/UserService.java
@@ -2,6 +2,7 @@ package com.tngtied.triplaner.service;
 
 import com.tngtied.triplaner.JwtAuthenticationFilter;
 import com.tngtied.triplaner.JwtTokenProvider;
+import com.tngtied.triplaner.UserRole;
 import com.tngtied.triplaner.dto.TokenInfo;
 import com.tngtied.triplaner.entity.Member;
 import com.tngtied.triplaner.repository.UserRepository;
@@ -12,9 +13,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,19 +30,22 @@ import org.springframework.transaction.annotation.Transactional;
 import java.awt.*;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-@Transactional(readOnly =true)
+@Transactional
 @SpringBootApplication
-public class UserService {
+public class UserService implements UserDetailsService{
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
     private final JwtTokenProvider jwtTokenProvider;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
 //    @Bean
 //    public FilterRegistrationBean jwtFilter(){
@@ -70,12 +81,41 @@ public class UserService {
         // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
         // authenticate 매서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드가 실행
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        //authenticationManagerBuilder.getObject() 부분이 null인 문제 지속 발생중
         System.out.println(">> Password check clear");
 
         // 3. 인증 정보를 기반으로 JWT 토큰 생성
         TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
 
         return tokenInfo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> user = this.userRepository.findByUsername(username);
+        if (user.isEmpty()){
+            throw new UsernameNotFoundException(">> Username not found.");
+        }
+        Member siteUserFound = user.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if ("admin".equals(siteUserFound.getUsername())){
+            authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
+        }else{
+            authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
+        }
+        System.out.println(">> loadUserByUsername() called, found successfully");
+
+        return new User(siteUserFound.getUsername(), siteUserFound.getPassword(), authorities);
+
+    }
+
+    private UserDetails createUserDetails(Member member){
+        return User.builder()
+                .username(member.getUsername())
+                .password(member.getPassword())
+                .roles(member.getRoles().toArray(new String[0]))
+                .build();
     }
 
 }
